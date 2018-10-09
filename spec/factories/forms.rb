dbd063705d82239f4c2880a8070a20c2bb5d4c99
Andre Morganti <acmorganti@gmail.com>
2018-10-09 09:47:57 -0300

    onebitform

---
+++ spec/factories/forms.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :form do
+    title { FFaker::Lorem.phrase }
+    description { FFaker::Lorem.phrase }
+    user
+    primary_color { "##{FFaker::Color.hex_code}" }
+    enable { FFaker::Boolean.maybe }
+  end
+end
